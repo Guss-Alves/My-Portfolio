ba97ef9994fe67b16266b8468a2cefa6f3ccfa9f
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2023-03-28 21:01:03 -0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/Portfolio",
+    base: "/My-Portfolio",
     plugins: [react()],
     build:{
         chunkSizeWarningLimit: 100000000,
